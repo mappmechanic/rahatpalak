b8948cb0a6abff9dcd8fc1c820b87b80b9af5257
RAHUL LUTHRA <rahulluthra90@gmail.com>
2014-06-11 00:34:03 +0530

    Navigation & 2 Views Done

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,14 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider
+  .when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'})
+  .when('/story', {templateUrl: 'partials/story.html', controller: 'StoryCtrl'})
+  .when('/familyfriends', {templateUrl: 'partials/familyfriends.html', controller: 'FamilyFriendsCtrl'})
+  .when('/events', {templateUrl: 'partials/events.html', controller: 'EventsCtrl'})
+  .when('/blessings', {templateUrl: 'partials/blessings.html', controller: 'BlessingsCtrl'})
+  .when('/gallery', {templateUrl: 'partials/gallery.html', controller: 'GalleryCtrl'})
+  .when('/rsvp', {templateUrl: 'partials/rsvp.html', controller: 'RsvpCtrl'});
+    
+  $routeProvider.otherwise({redirectTo: '/home'});
 }]);
